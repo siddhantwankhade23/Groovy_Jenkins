8f9d54dfe93a63a52416f323f61ce1b11911d0ee
Siddhant <siddhantwankhade23@gmail.com>
2021-12-16 16:11:02 +0530

    Initial Commit

--- src/Hello.groovy
+++ src/Hello.groovy
@@ -1,3 +1,7 @@
-print("Java Version  :- " + System.getProperty("java.version"))
-String environmentPath = Environment.GetEnvironmentVariable("JAVA_HOME");
-print("Path :-" + environmentPath);
+stage("Init") {
+    print("Java Version  :- " + System.getProperty("java.version"))
+    String environmentPath = Environment.GetEnvironmentVariable("JAVA_HOME");
+    print("Path :-" + environmentPath);
+
+    Check.getCommandOutput("where java")
+}
